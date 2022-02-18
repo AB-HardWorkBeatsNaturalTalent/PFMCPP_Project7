d4449f805080259350438e146914005b67fc25ca
Adam <AB-HardWorkBeatsNaturalTalent@users.noreply.github.com>
2022-02-18 23:15:40 +0000

    removed assert statements. need to update the dragonslayer attack method. and also to add utility methods for making and getting the attack items.

--- DragonSlayer.cpp
+++ DragonSlayer.cpp
@@ -23,7 +23,7 @@ void DragonSlayer::attack(Character& other)
     std::cout << name << " is attacking " << other.getName() << " !!" << std::endl;
     if( auto* dragon = dynamic_cast<Dragon*>(&other) )
     {
-        assert(false);
+        //assert(false);
 
     //note: they should only use the item if the dragon's hitpoints are > 0...  
         while( dragon->getHP() > 0 )
@@ -34,6 +34,7 @@ void DragonSlayer::attack(Character& other)
             //for each item in this characters list of attack items, 
                 //use the item, 
                 //reset the item.
+                
             //note: items are single-use only, so you need to reset it after use.  
             //look in the Character class for how the other item types are reset after use.  
             dragon->takeDamage(attackDamage);
